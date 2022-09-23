314c14afa6bcfc34877712b111e79435da31cdf5
CyskoFR <franc.sko91@gmail.com>
2022-09-23 15:12:49 +0200

    add address classes + console stamp

---
+++ Address.cs
@@ -0,0 +1,34 @@
+﻿//In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+public class Address
+{
+    public string Name;
+    public string Surname;
+    public string Street;
+    public string City;
+    public string Province;
+    public string Zip;
+    public Address(string name, string surname, string street, string city, string province, string zip)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Street = street;
+        this.City = city;
+        this.Province = province;
+        this.Zip = zip;
+    }
+    public override string ToString()
+    {
+        string line = "";
+        line += "Name:\t" + this.Name + "\n";
+        line += "Surname:\t" + this.Surname + "\n";
+        line += "Street:\t" + this.Street + "\n";
+        line += "City:\t" + this.City + "\n";
+        line += "Province:\t" + this.Province + "\n";
+        line += "ZIP:\t" + this.Zip + "\n";
+        line += "-------------------";
+
+        return line;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿//In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
 //Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+StreamReader addresses = File.OpenText("../../../addresses.csv");
+
+List<Address> addressList = new List<Address>();
+
+addresses.ReadLine();
+
+while (!addresses.EndOfStream)
+{
+    string line = addresses.ReadLine();
+
+    string[] split = line.Split(',');
+
+    try
+    {
+        string name = split[0];
+        string surname = split[1];
+        string street = split[2];
+        string city = split[3];
+        string province = split[4];
+        string zip = split[5];
+        Address address = new Address(name, surname, street, city, province, zip);
+        addressList.Add(address);
+        Console.WriteLine(address);
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("Invalid address --> " + line);
+    }
+}
+
+addresses.Close();
